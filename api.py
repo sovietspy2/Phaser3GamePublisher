550fea517aaa3ad37f28f26abbddb62bbe423644
SovietSpy2 <sovietspy2@yandex.com>
2018-11-24 16:51:19 +0000

    edits

--- api.py
+++ api.py
@@ -2,7 +2,7 @@
 from flask import Flask
 import subprocess
 import logging
-import time;    
+import time
 
 logging.basicConfig(filename='/home/sovietspy2/log/api.log',level=logging.INFO)
 
@@ -11,9 +11,13 @@ app = Flask(__name__)
 @app.route('/publish')
 def index():
     current_time = time.asctime( time.localtime(time.time()) )
-    subprocess.call("git pull")
-    subprocess.call("npm build")
-    logging.info('Pull @ '+current_time)
+    subprocess.call("git -C /home/sovietspy2/www/phaser3Game/ pull")
+    logging.info('Pulled . . .')
+    subprocess.call("npm install --prefix /home/sovietspy2/www/phaser3Game/")
+    logging.info('NPM install . . .')
+    subprocess.call("npm run build --prefix /home/sovietspy2/www/phaser3Game/")
+    logging.info('NPM build . . . ')
+    logging.info('finished @ '+current_time)
     return "OK"
 
 if __name__ == '__main__':
